74387e8517c200998382a0f3a26f59f02ad64bf0
Ariya Phengphon <81862069+ChanAndKlee@users.noreply.github.com>
2025-06-21 14:36:43 +0000

    bootcamp module 1 (revised)

--- 00-bootcamp-project/main-api.py
+++ 00-bootcamp-project/main-api.py
@@ -1,27 +1,33 @@
-import configparser
 import csv
-
+import configparser
 import requests
 
-
+# Read config from config file (Don't hardcode credentials)
 parser = configparser.ConfigParser()
 parser.read("pipeline.conf")
 host = parser.get("api_config", "host")
 port = parser.get("api_config", "port")
 
 API_URL = f"http://{host}:{port}"
+
+# Define destination folder
 DATA_FOLDER = "data"
 
 ### Events
 data = "events"
 date = "2021-02-10"
 response = requests.get(f"{API_URL}/{data}/?created_at={date}")
+print(f"({data}) Request URL: {f"{API_URL}/{data}/?created_at={date}"}")
 data = response.json()
+
+# Write data to CSV file
 with open(f"{DATA_FOLDER}/events.csv", "w") as f:
     writer = csv.writer(f)
     header = data[0].keys()
+    # Write header from first row's key
     writer.writerow(header)
 
+    # Write data to CSV file
     for each in data:
         writer.writerow(each.values())
 
@@ -31,12 +37,17 @@ date = "2020-10-23"
 # ลองดึงข้อมูลจาก API เส้น users และเขียนลงไฟล์ CSV
 # YOUR CODE HERE
 response = requests.get(f"{API_URL}/{data}/?created_at={date}")
+print(f"({data}) Request URL: {f"{API_URL}/{data}/?created_at={date}"}")
 data = response.json()
+
+# Write data to CSV file
 with open(f"{DATA_FOLDER}/users.csv", "w") as f:
     writer = csv.writer(f)
     header = data[0].keys()
+    # Write header from first row's key
     writer.writerow(header)
 
+    # Write data to CSV file
     for each in data:
         writer.writerow(each.values())
 
@@ -46,11 +57,16 @@ date = "2021-02-10"
 # ลองดึงข้อมูลจาก API เส้น orders และเขียนลงไฟล์ CSV
 # YOUR CODE HERE
 response = requests.get(f"{API_URL}/{data}/?created_at={date}")
+print(f"({data}) Request URL: {f"{API_URL}/{data}/?created_at={date}"}")
 data = response.json()
+
+# Write data to CSV file
 with open(f"{DATA_FOLDER}/orders.csv", "w") as f:
     writer = csv.writer(f)
     header = data[0].keys()
+    # Write header from first row's key
     writer.writerow(header)
 
+    # Write data to CSV file
     for each in data:
         writer.writerow(each.values())
--- 00-bootcamp-project/main-sftp.py
+++ 00-bootcamp-project/main-sftp.py
@@ -1,8 +1,7 @@
 import configparser
-
 import pysftp
 
-
+# Read config from config file (Don't hardcode credentials)
 parser = configparser.ConfigParser()
 parser.read("pipeline.conf")
 username = parser.get("sftp_config", "username")
@@ -10,22 +9,28 @@ password = parser.get("sftp_config", "password")
 host = parser.get("sftp_config", "host")
 port = parser.getint("sftp_config", "port")
 
-
 # Security risk! Don't do this on production
 # You lose a protection against Man-in-the-middle attacks
 cnopts = pysftp.CnOpts()
 cnopts.hostkeys = None
 
+# Define destination folder
 DATA_FOLDER = "data"
 
 # โค้ดด้านล่างจะเป็นการโหลดไฟล์ orders.csv ซึ่งเป็นข้อมูล orders
 # ให้แก้โค้ดด้านล่างให้ไปโหลดไฟล์ข้อมูล products และ promos แทน
 files = [
     "orders.csv",
+    # YOUR CODE HERE
     "products.csv",
     "promos.csv"
 ]
+
+# Destination path (SFTP directory)
+REMOTE_FOLDER = "/"
+
+# Get/download files from SFTP server
 with pysftp.Connection(host, username=username, password=password, port=port, cnopts=cnopts) as sftp:
     for f in files:
-        sftp.get(f, f"{DATA_FOLDER}/{f}") # Destination path
+        sftp.get(f"{REMOTE_FOLDER}/{f}", f"{DATA_FOLDER}/{f}")
         print(f"Finished downloading: {f}")
